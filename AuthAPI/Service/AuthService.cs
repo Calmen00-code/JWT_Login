24d2815fe6d107207fd3a489b68dd331a48af82c
Calmen <calmenchia@gmail.com>
2025-06-29 11:09:21 +0800

    Configure Authentication service with neccessary DI

---
+++ AuthAPI/Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using LoginJWT.Services.AuthAPI.Data;
+using LoginJWT.Services.AuthAPI.DTO;
+using LoginJWT.Services.AuthAPI.Models;
+using LoginJWT.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace LoginJWT.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO> Register(RegistrationRequestDTO newUserRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
