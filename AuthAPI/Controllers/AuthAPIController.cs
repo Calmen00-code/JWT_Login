40a4ca9fd77115611896249440b0cb244d0d5399
Calmen <calmenchia@gmail.com>
2025-06-29 10:49:46 +0800

    Added endpoints of the API in controller

---
+++ AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginJWT.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
