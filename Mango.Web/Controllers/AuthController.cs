a2eda29ee39247aa8cc1a35258b6b01879508f3c
Calmen <calmenchia@gmail.com>
2025-07-01 22:56:29 +0800

    Login and Register in action

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Mango.Web.Utility;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
@@ -34,6 +35,44 @@ namespace Mango.Web.Controllers
 
             ViewBag.RoleList = roleList;
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDTO loginRequestDTO)
+        {
+            ResponseDTO? response = await _authService.LoginAsync(loginRequestDTO);
+
+            if (response != null && response.IsSuccess)
+            {
+                LoginResponseDTO? loginResponse =
+                    JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(response.Result));
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(loginRequestDTO);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDTO registrationRequest)
+        {
+            ResponseDTO? response = await _authService.RegisterAsync(registrationRequest);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem { Text=SD.RoleAdmin, Value = SD.RoleAdmin },
+                new SelectListItem { Text=SD.RoleCustomer, Value = SD.RoleCustomer }
+            };
+
+            ViewBag.RoleList = roleList;
+            return View(registrationRequest);
         }
 
         [HttpGet]
--- Mango.Web/Models/DTO/RegistrationRequestDTO.cs
+++ Mango.Web/Models/DTO/RegistrationRequestDTO.cs
@@ -1,10 +1,17 @@
-﻿namespace Mango.Web.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models.DTO
 {
     public class RegistrationRequestDTO
     {
+        [EmailAddress]
         public string Email { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public string Role { get; set; }
     }
