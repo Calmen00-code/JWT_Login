082f6d138dd1ca5441b77fda31f73bdff616451c
Calmen <calmenchia@gmail.com>
2025-07-01 21:52:07 +0800

    Login, Register, Logout endpoint at AuthController

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Mango.Web.Models.DTO;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO(); 
+            return View(loginRequestDTO);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
