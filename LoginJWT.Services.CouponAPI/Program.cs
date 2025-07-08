46963ed1529c06981b25565de8d9947ed0e06bb4
Calmen <calmenchia@gmail.com>
2025-07-08 23:12:54 +0800

    IMPORTANT: Cleanup Program.cs in CouponAPI

--- LoginJWT.Services.CouponAPI/Program.cs
+++ LoginJWT.Services.CouponAPI/Program.cs
@@ -47,30 +47,7 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
-
-var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
-var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
-var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
-
-var key = Encoding.ASCII.GetBytes(secret);
-
-builder.Services.AddAuthentication(u =>
-{
-    u.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    u.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(u =>
-{
-    u.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidIssuer = issuer,
-        ValidateAudience = true,
-        ValidAudience = audience
-    };
-});
-
+builder.AddAppAuthentication();
 builder.Services.AddAuthentication();
 
 var app = builder.Build();
